4c1ad552fae54655341c4b30a1128aab8e47faf7
Scopic <scopic@Buddhikas-MacBook-Air.local>
2022-06-22 21:27:36 +0530

    making transcript text left alligned

--- src/components/Transcript.js
+++ src/components/Transcript.js
@@ -3,8 +3,8 @@ import { Card } from "react-bootstrap";
 const Transcript = (props) => {
     return <Card className="mt-3 mb-3 p-4 mx-auto w-75">
         {props.transcript?.map((addressing) => <>
-            <p className="fw-bold">{addressing[0]}</p>
-            <p>{addressing[1]}</p>
+            <p className="fw-bold text-start">{addressing[0]}</p>
+            <p className="text-start">{addressing[1]}</p>
             <hr class="mt-2 mb-3"/>
         </>)}
     </Card>
