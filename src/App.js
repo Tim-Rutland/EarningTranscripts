b46942089cc70b6a4ad8ea7b8273042a67933f37
Scopic <scopic@Buddhikas-MacBook-Air.local>
2022-06-22 19:19:58 +0530

    error handling and showing loading indicator

--- src/App.js
+++ src/App.js
@@ -4,18 +4,37 @@ import NavBar from './components/Navbar';
 import SearchForm from './components/SearchForm';
 import Transcript from './components/Transcript';
 import { getTrascript } from './services/api.service';
+import { formatTranscriptContent } from './services/formatter.service';
+import { Alert, Spinner } from 'react-bootstrap';
 
 function App() {
   const [transcript, setTranscript] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   return (
     <div className="App">
       <NavBar />
+      {error && <Alert variant="danger" onClose={() => setError(null)} dismissible>
+        <Alert.Heading>{error}</Alert.Heading>
+      </Alert>}
       <SearchForm
         onSubmit={
-          async (symbol, year, quarter) => setTranscript(await getTrascript(symbol, year, quarter))
+          async (symbol, year, quarter) => {
+            setLoading(true);
+            const content = await getTrascript(symbol, year, quarter);
+            if (!content?.length) {
+              setTranscript(null);
+              setError('No Data Found');
+            } else {
+              setTranscript(formatTranscriptContent(content));
+              setError(null);
+            }
+            setLoading(false);
+          }
         } />
-      <Transcript transcript={transcript} />
+      {loading && <Spinner className="mt-5" animation="grow" />}
+      {!loading && transcript && <Transcript transcript={transcript} />}
     </div>
   );
 }
--- src/components/Transcript.js
+++ src/components/Transcript.js
@@ -1,9 +1,8 @@
 import { Card } from "react-bootstrap";
-import { formatTranscriptContent } from "../services/formatter.service";
 
 const Transcript = (props) => {
     return <Card className="mt-3 mb-3 p-4 mx-auto w-75">
-        {formatTranscriptContent(props.transcript).map((addressing) => <>
+        {props.transcript?.map((addressing) => <>
             <p className="fw-bold">{addressing[0]}</p>
             <p>{addressing[1]}</p>
             <hr class="mt-2 mb-3"/>
